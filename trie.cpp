c5bc64a722646753f448654de36730cebd2c90fa
Avi Tevet <avi@avitevet.com>
2017-03-12 00:41:17 -0800

    Implement insert & contains specific to std::string, instead of using c-string implementations via string::c_str()

--- trie.cpp
+++ trie.cpp
@@ -5,7 +5,26 @@ trie::trie() :
 {}
 
 void trie::insert(const std::string &s) {
-	insert(s.c_str());
+	int currentNode = 0;
+	int childNode = 0;
+	int childIndex = 0;
+
+	for (std::string::const_iterator it = s.begin(); it != s.end(); ++it) {
+		childIndex = TrieNode::toChildIndex(*it);
+		childNode = nodes[currentNode].children[childIndex];
+		if (childNode != -1) {
+			currentNode = childNode;
+		}
+		else {
+			// create a new node
+			nodes[currentNode].children[childIndex] = nodes.size();
+			currentNode = nodes.size();
+			nodes.emplace_back();
+		}
+	}
+
+	// allows the root node to be a leaf if the empty string is inserted
+	nodes[currentNode].isLeaf = true;
 }
 
 void trie::insert(const char *s) {
@@ -32,7 +51,23 @@ void trie::insert(const char *s) {
 }
 
 bool trie::contains(const std::string &s) const {
-	return contains(s.c_str());
+	int currentNode = 0;
+	int childNode = 0;
+	int childIndex = 0;
+
+	for (std::string::const_iterator it = s.begin(); it != s.end(); ++it) {
+		childIndex = TrieNode::toChildIndex(*it);
+		childNode = nodes[currentNode].children[childIndex];
+		if (childNode == -1) {
+			return false;
+		}
+		else {
+			// go to child node
+			currentNode = childNode;
+		}
+	}
+
+	return nodes[currentNode].isLeaf;
 }
 
 bool trie::contains(const char *s) const {
